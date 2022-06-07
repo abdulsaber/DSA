9b2f3986ae538ed7ff410a5cde9d5cced29c29d3
Abdul saber <abdulsaber.as@gmail.com>
2022-06-07 13:25:04 +0530

    6.Solution

---
+++ Day-2/Smallestno.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Smallestno {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number: ");
+        double x = in.nextDouble();
+        System.out.print("Input the Second number: ");
+        double y = in.nextDouble();
+        System.out.print("Input the third number: ");
+        double z = in.nextDouble();
+        System.out.print("The smallest value is " + smallest(x, y, z)+"\n" );
+    }
+
+   public static double smallest(double x, double y, double z)
+    {
+        return Math.min(Math.min(x, y), z);
+    }
+    
+}
