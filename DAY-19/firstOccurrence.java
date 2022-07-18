79b68a867230229a65a50f03fc4c66ac728b885c
Abdul saber <abdulsaber.as@gmail.com>
2022-07-18 16:19:18 +0530

    2. Solution

---
+++ DAY-19/firstOccurrence.java
@@ -0,0 +1,25 @@
+public class firstOccurrence {
+    public static void main(String[] args) {
+        int a[] = {1,1,1,2,3,4,2,4,6,8,9};
+        int key = 2;
+        System.out.println(firstOccurrence(a, key));
+    }
+
+    static int firstOccurrence(int[] a, int key){
+        int l = 0;
+        int r = a.length-1;
+        int ans = -1;
+        while(l <= r){
+            int mid = (l+r)/2;
+            if(a[mid]==key){
+                ans = mid;
+                r = mid -1;
+            }
+            else if(a[mid] < key) l++;
+            else r--;
+        }
+        return ans;
+    }
+    
+    
+}
