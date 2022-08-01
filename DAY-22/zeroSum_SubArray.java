acaf8733e2fb6170cd44f5b3e567b68cd28aa500
Abdul saber <abdulsaber.as@gmail.com>
2022-08-01 17:13:49 +0530

    8. Solution

--- DAY-22/zeroSum_SubArray.java
+++ DAY-22/zeroSum_SubArray.java
@@ -2,20 +2,22 @@ import java.util.*;
 
 public class zeroSum_SubArray {
     public static void main(String[] args) {
-        int[] a = {1,2,6,-6,5};
+        int[] a = {-1,2,6,-6,5};
         System.out.println(zeroSum_SubArray(a));
 
     }
     static int zeroSum_SubArray(int[] a){
         HashMap<Integer, Integer> map = new HashMap<>();
-        int cs = 0,i = 1,curLen = 0;
-        map.put(0,0);
-        for (int value : a) {
+        int cs = 0,curLen = 0;
+        map.put(0,-1);
+        for (int i = 0; i < a.length; i++) {
+            int value = a[i];
             cs += value;
             if(map.containsKey(cs)){
-                curLen = i- map.get(cs);
+                int prev = map.get(cs);
+                curLen = Math.max(curLen, i-prev);
             } 
-            else map.put(value,i++ );
+            else map.put(value, i);
         }
         return curLen;
     }
