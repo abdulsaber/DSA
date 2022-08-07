a28401c06eca892c912ddf3277372c25ffeefae0
Abdul saber <abdulsaber.as@gmail.com>
2022-08-07 20:18:45 +0530

    5. Solution

---
+++ DAY-27/previousSmallerNumber.java
@@ -0,0 +1,34 @@
+import java.util.ArrayDeque;
+
+public class previousSmallerNumber {
+    public static void main(String[] args) {
+        int a[] = {5, 1, 2, 3, 9, 6, 1, 4, 3};
+		int ans[] = smallerelements(a);
+		for(int i = 0; i<a.length; i++) {
+			System.out.print(a[i]+" ");
+			System.out.print(ans[i]+" ");
+			System.out.println();
+		}
+    }
+
+    static int[] smallerelements(int[] a){
+        int[] ans = new int[a.length];
+        int i = 0;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for(int e: a){
+            while(!stack.isEmpty() && stack.peek() >= e){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[i] = -1;
+            }else{
+                ans[i] = stack.peek();    
+            }
+            stack.push(e);
+            i++;
+        }
+        return ans;
+    }
+    
+}
