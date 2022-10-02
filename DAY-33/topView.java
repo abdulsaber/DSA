5566f96206e25e443b19b8ba4ef9aa1575713246
Abdul saber <abdulsaber.as@gmail.com>
2022-10-02 20:52:58 +0530

    3. Solution

---
+++ DAY-33/topView.java
@@ -0,0 +1,34 @@
+public class topView {
+    static ArrayList<Integer> topView(Node root){
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Queue<Pair> q = new ArrayDeque<>();
+        Map<Integer,Integer> map = new TreeMap<>();
+        
+        q.add(new Pair(0, root));
+        while(!q.isEmpty()){
+            Pair cur = q.poll();
+            int hd = cur.hd;
+            if(!map.containsKey(hd)){
+                map.put(hd,cur.node.data);
+                
+            }
+            if(cur.node.left != null) q.add(new Pair(hd-1,cur.node.left));
+            if(cur.node.right != null) q.add(new Pair(hd+1,cur.node.right));
+        }
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            list.add(entry.getValue());
+        }
+        return list;
+       
+   }
+   static class Pair{
+       int hd;
+       Node node;
+       public Pair(int hd, Node node){
+           this.hd = hd;
+           this.node = node;
+       }
+       
+   }
+}
