2465233c7d37bb6f5e881e1f4ea31f311f185c2d
Abdul saber <abdulsaber.as@gmail.com>
2022-12-08 23:11:48 +0530

    2. Solution

--- DAY-41/kSubset.java
+++ DAY-41/kSubset.java
@@ -1,32 +1,33 @@
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class kSubset {
+    public static void main(String[] args) {
+        int a[] = {1, 4, 2, 3, 7, 2, 4, 5, 6, 3};
+		int k = 3;
+		
+		System.out.println(splitArrayInKSubsets(a, k));
+    }
 
-    PriorityQueue<Integer> minHeap, maxHeap;
-    boolean even;
-    
-    public MedianFinder() {
-        minHeap = new PriorityQueue<>();
-        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        even = true;
+    public static int splitArrayInKSubsets(int[] a, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i = 0; i<k; i++) {
+			pq.offer(0);
+		}
+		Arrays.sort(a);
+		for(int i = a.length-1; i>=0; i--) {
+			int cur = a[i];
+			int top = pq.poll();
+			int toAdd = cur + top;
+			pq.offer(toAdd);
+		}
+		int max = -1;
+		for(int e: pq) {
+			max = Math.max(max, e);
+		}
+		return max;
     }
     
-    public void addNum(int num) {
-        if(even) {
-            minHeap.offer(num);
-            maxHeap.offer(minHeap.poll());
-        } else {
-            maxHeap.offer(num);
-            minHeap.offer(maxHeap.poll());
-        }
-        even = !even;
-    }
     
-    public double findMedian() {
-        if(even) {
-            return (minHeap.peek() + maxHeap.peek())/2.0;
-        } else {
-            return (double)maxHeap.peek();
-        }
-    }
 }
