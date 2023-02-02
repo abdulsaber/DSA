62b059aebd39b63f0ec8291bc8a0ed1242cdc139
Abdul saber <abdulsaber.as@gmail.com>
2023-02-02 17:42:43 +0530

    2. Solution

---
+++ DAY-49/rodCutting.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class rodCutting {
+    static int rod(int[] prices, int n){
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+
+        rodDP(prices, n, dp);
+        return dp[n];
+    }    
+
+    static int rodDP(int[] prices, int n, int[] dp){
+        if(n < 0) return -100000;
+        if(dp[n] != -1) return dp[n];
+        if(n == 0) return 0;
+
+        int max = -1;
+        for(int i = 0; i < prices.length; i++){
+            max = Math.max(max, rodDP(prices, n-i-1, dp));
+        }
+        return dp[n] = max;
+    }
+}
